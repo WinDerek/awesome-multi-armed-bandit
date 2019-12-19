db478bdbebf944593ec588b611bc0072c1936451
derek <frankderekdick@gmail.com>
2019-12-19 20:15:52 +0800

    Moves the agents into the agent.py file.

--- bandit_project/agent.py
+++ bandit_project/agent.py
@@ -1,4 +1,5 @@
 import numpy as np
+from machine import get_reward
 
 
 class Agent:
@@ -20,3 +21,37 @@ class Agent:
 
     def name(self):
         return 'agent'
+
+
+class EpsilonGreedyAgent(Agent):
+    def __init__(self, epsilon, **kwds):
+        self.epsilon = epsilon
+
+        self.reset()
+
+        super().__init__(**kwds)
+
+
+    def reset(self):
+        self.theta_hat_array = np.zeros(3, dtype=float)
+        self.count_array = np.zeros(3, dtype=int)
+
+    
+    def pull_arm(self):
+        I = -1
+        # Choose I randomly with probability epsilon
+        if np.random.binomial(1, self.epsilon):
+            I = np.random.randint(1, 4)
+        # Get greedy with probability (1 - epsilon)
+        else:
+            I = np.argmax(self.theta_hat_array) + 1
+        reward = get_reward(I)
+
+        self.count_array[I-1] += 1
+        self.theta_hat_array[I-1] += (reward - self.theta_hat_array[I-1]) / self.count_array[I-1]
+
+        return I, reward
+
+
+    def name(self):
+        return '$\epsilon$-greedy with $\epsilon = {}$'.format(self.epsilon)
